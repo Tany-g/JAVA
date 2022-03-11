78e2b61240759d3f235025a6e2f93a544168de06
Tany-g <1767774755@qq.com>
2022-03-11 10:38:51 +0800

    IDEA测试

---
+++ src/Imput.java
@@ -0,0 +1,12 @@
+import com.sun.org.apache.bcel.internal.generic.NEW;
+
+import java.util.Scanner;
+public class Imput
+{
+    public static void main(String[] arg)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println(in.nextLine());
+    }
+
+}
